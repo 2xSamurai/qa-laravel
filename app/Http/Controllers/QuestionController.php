e9f1a03d4d610f1b7ac731204b9320120ee392b7
Deepak Vijayan <deepak@awsm.in>
2022-04-07 10:45:31 +0530

    login, register, logout

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Question;
+use Illuminate\Http\Request;
+
+class QuestionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+        return Question::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        $request->validate([
+            'question' => 'required'
+        ]);
+
+        return Question::create($request->all());
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+        return Question::find($id);
+        // return Question::where('question_id', '=', $id)->firstOrFail();    
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        // $question = Question::where('question_id', '=', $id)->firstOrFail();    
+        $question = Question::find($id);
+        $question->update($request->all());
+        return $question;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        return Question::destroy();
+    }
+
+    /**
+     * Search for a question.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function search($question)
+    {
+        // return $question;
+        //
+        return Question::where('question', 'like', '%'.$question.'%')->get();
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +14,18 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Protected Routes
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/questions/search/{question}', [QuestionController::class, 'search']);
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+//Public Routes
+Route::resource('questions', QuestionController::class);
+// Route::get('/questions/search/{question}', [QuestionController::class, 'search']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+// Route::post('/questions', [QuestionController::class, 'store']);
+// Route::get('/questions', [QuestionController::class, 'index']);
